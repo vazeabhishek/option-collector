e1af4cd34f9e2e0f94b17346dadb68295a084747
Abhishek Vaze <abhishek.vaze1@ingka.com>
2021-01-19 12:48:31 +0100

    changed db

--- src/main/db/fn/calculate15mAnalytics.sql
+++ src/main/db/fn/calculate15mAnalytics.sql
@@ -1,17 +1,19 @@
-CREATE OR REPLACE FUNCTION FN_15MINS_ANALYTICS()
+CREATE OR REPLACE FUNCTION option.calcAndStoreAnalytics()
   RETURNS TRIGGER
   LANGUAGE PLPGSQL
   AS
 $$
 DECLARE
 v_sentiment character varying(255);
+v_signal character varying(255);
 v_trend double precision;
 v_strength double precision;
 v_ltp double precision;
+latest record;
 BEGIN
 	IF NEW.COUNTER % 5 = 0
 	THEN
-         select * from option.calculateOptionTrendInInterval(NEW.COUNTER,(NEW.COUNTER - 4),NEW.option_option_record_no)
+         select * from option.calculateTrendInInterval(NEW.COUNTER,(NEW.COUNTER - 4),NEW.option_option_record_no)
          INTO v_trend,v_strength,v_ltp;
          IF v_trend > 0 AND v_strength > 0
          THEN
@@ -29,8 +31,25 @@ BEGIN
           THEN
            v_sentiment := 'WEAK BEARISH';
          END IF;
-		 INSERT INTO option.OPTION_15M_A(collection_time,ltp,option_option_record_no,trend,strength,sentiment)
-		 VALUES(NEW.collection_time,v_ltp,NEW.option_option_record_no,v_trend,v_strength,v_sentiment);
+
+         SELECT collection_time,ltp,option_option_record_no,trend,strength,sentiment FROM option.OPTION_15M_A
+         WHERE option_option_record_no = NEW.option_option_record_no
+         ORDER BY collection_time desc LIMIT 1
+         INTO latest;
+
+         v_signal := 'WAIT';
+         IF latest.sentiment = v_sentiment AND v_sentiment = 'STRONG BULLISH'
+            THEN
+             v_signal := 'BUY';
+         END IF;
+
+         IF latest.sentiment = v_sentiment AND v_sentiment = 'STRONG BEARISH'
+            THEN
+             v_signal := 'SELL';
+         END IF;
+
+		 INSERT INTO option.EQUITY_15M_A(collection_time,ltp,equity_equity_record_no,trend,strength,sentiment,signal)
+		 VALUES(NEW.collection_time,v_ltp,NEW.equity_equity_record_no,v_trend,v_strength,v_sentiment,v_signal);
 	END IF;
 	RETURN NEW;
 END;
