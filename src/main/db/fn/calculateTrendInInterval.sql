d2fdcf02467c989ccddf1b6326c608287bd9ee47
Abhishek Vaze <abhishek.vaze1@ingka.com>
2021-01-20 11:32:20 +0100

    fixed trend function

--- src/main/db/fn/calculateTrendInInterval.sql
+++ src/main/db/fn/calculateTrendInInterval.sql
@@ -15,7 +15,7 @@ $$
     counter_interval = counter_end - counter_start + 1;
 
     select sum((ABS(P_DELTA_LTP_WRT_PREV) * trend_indicator))/counter_interval as trend,
-    sum((ABS(OI_CHG_WRT_PREV) * trend_strength))/counter_interval as strength,sum()/counter_interval as avgLtp
+    sum((ABS(OI_CHG_WRT_PREV) * trend_strength))/counter_interval as strength,sum(latest_price)/counter_interval as avgLtp
     into trend,strength,avgLtp
     from option.option_detail_history where option_option_record_no = option_contract_no
     and counter between counter_start and counter_end;
